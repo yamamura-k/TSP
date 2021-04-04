f15893b9620f84925ace12e1a5bd2a1ad0d49427
yamamura-k <yamayama23bb@gmail.com>
2021-04-04 22:03:19 +0900

    update

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 def read(filename):
     coord = dict()
     D = []
+    _type = None
     with open(filename, "r") as f:
         for line in f:
             line = line.rstrip()
@@ -19,9 +20,8 @@ def read(filename):
             elif line[0] == "EDGE_WEIGHT_SECTION":
                 D = [ _line.split() for _line in f]
                 D = list(list(map(float, x))for x in D[:-1])
-            elif line[0] == "EDGE_WEIGHT_TYPE" and line[1] != "EUC_2D":
-                print("Cannot use this problem file.")
-                return None
+            elif line[0] == "EDGE_WEIGHT_TYPE":
+                _type = line[1]
             elif line[0] == "NODE_COORD_SECTION":
                 for _line in f:
                     if _line[:3] != "EOF":
