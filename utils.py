d4c4e47068d3237ec2e065f13ee9e99e8ab25fe3
yamamura-k <yamayama23bb@gmail.com>
2021-04-04 20:33:38 +0900

    読み込み更新

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#from scipy.spatial.distance import cdist
+from scipy.spatial.distance import cdist
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
@@ -10,34 +10,32 @@ def read(filename):
         for line in f:
             line = line.rstrip()
             line = line.split(":")
-            print(line)
-            if line[0][:-1] == "NAME":
-                name = line[1][1:]
-                print(line)
+            if "NAME" in line[0]:
+                name = line[1]
             elif line[0] == "DIMENSION":
                 ncity = int(line[1])
-                print(line)
             elif line[0] == "EDGE_WEIGHT_SECTION":
                 D = [ _line.split() for _line in f]
                 D = list(list(map(float, x))for x in D[:-1])
-                print(line)
-            elif line[0][:-1] == "EDGE_WEIGHT_TYPE" and line[1][1:] != "EUC_2D":
+            elif "EDGE_WEIGHT_TYPE" in line[0] and "EUC_2D" not in line[1]:
                 print("Cannot use this problem file.")
                 return None
             elif line[0] == "NODE_COORD_SECTION":
-                print(line)
                 for _line in f:
                     if _line[:3] != "EOF":
                         _line = list(map(float, _line.split()))
                         coord[_line[0]] = _line[1:]
+                    else:
+                        break
     if coord and not D:
-        #D = cdist(coord.values(), coord.values())
+        D = cdist(coord.values(), coord.values())
+        """
         for v in coord.values():
             tmp = []
             for u in coord.values():
                 tmp.append(sqrt(pow(u[0] - v[0], 2) + pow(u[1] - v[1], 2)))
             D.append(tmp)
-
+        """
     return name, ncity, D, coord
 
 def plot(tour, coord, figname="./tmp.png"):
@@ -51,6 +49,6 @@ def calc_dist(tour, D):
     return sum(D[i][j] for i, j in zip(tour, tour[1:]+tour[:1]))
 
 if __name__=="__main__":
-    name, ncity, D, coord = read("./ALL_tsp/a280.tsp")
+    name, ncity, D, coord = read("./ALL_tsp/berlin52.tsp")
     tour = list(range(1, ncity + 1))
     plot(tour, coord, figname=f"./{name}.png")
